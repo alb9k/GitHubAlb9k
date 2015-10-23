ce2e4862b27deff542a451cd6a5fca0d702745f4
alb9k <alby9000@gmail.com>
2015-10-23 11:41:21 +0200

    Aggiunto: year
    Aggiunta variabile year e riformatazione classe

--- Profession.java
+++ Profession.java
@@ -4,34 +4,33 @@ import java.io.Serializable;
 
 public class Profession implements Serializable {
 
-
 	private static final long serialVersionUID = 1L;
-	
+
 	private String title;
-    private String industry;
-    private int year;
-    
-    public int getYear() {
-    	return year;
-    }
-    
-    public void setYear() {
-    	this.year = year;
-    }
- 
-    public String getTitle() {
-        return title;
-    }
- 
-    public void setTitle(String title) {
-        this.title = title;
-    }
- 
-    public String getIndustry() {
-        return industry;
-    }
- 
-    public void setIndustry(String industry) {
-        this.industry = industry;
-    }
+	private String industry;
+	private int year;
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear() {
+		this.year = year;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIndustry() {
+		return industry;
+	}
+
+	public void setIndustry(String industry) {
+		this.industry = industry;
+	}
 }
